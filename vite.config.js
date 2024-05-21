540c65d6d17554e5745d530d1cf935aba4e12882
JekabsVanags <jecobv@gmail.com>
2024-05-21 22:31:23 +0300

    Set href

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+function addPrefixToHref() {
+	return {
+		name: 'add-prefix-to-href',
+		enforce: 'post',
+		generateBundle(options, bundle) {
+			for (const [fileName, asset] of Object.entries(bundle)) {
+				if (fileName.endsWith('.html') && asset.type === 'asset') {
+					const html = asset.source.toString();
+					const modifiedHtml = html.replace(/href="\.\//g, 'href="/LU-Takas/');
+					bundle[fileName].source = modifiedHtml;
+				}
+			}
+		}
+	};
+}
+
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	// @ts-ignore
+	plugins: [sveltekit(), addPrefixToHref()]
 });
 
