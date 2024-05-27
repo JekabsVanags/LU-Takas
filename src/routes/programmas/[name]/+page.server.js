6ad1052116843037cf368cc4fff8938c88df53dd
JekabsVanags <jecobv@gmail.com>
2024-05-27 23:11:54 +0300

    Pls work

--- src/routes/programmas/[name]/+page.server.js
+++ src/routes/programmas/[name]/+page.server.js
@@ -7,21 +7,21 @@ export function load({ params }) {
       slug: "datoriki",
       name: "Datoriķi!",
       description: "Datorzinātnes ir programma kur iet tad ja kaut bišķi interesē datori, un nāktonē vēlies pelnīt lielas naudas! Ne velti kursu ikgadu sāk nu jau krietni vairāk kā 200 studentu, tomēr ņem vērā, kvalifikācijas darbu aizstāvēs labi ja puse. Par Bakalaura iegušanu nerunājot!",
-      img: `${base}/../trails.jpg`,
+      img: `../${base}/trails.jpg`,
       years: 4
     },
     {
       slug: "geografi",
       name: "Ģeogrāfi!",
       description: "Vislatvijas slavena mafija, kas maskējas par fakultāti. Ģeogrāfi vienmēr ir gatavi maukt ģeorallijā (t.i. laivu braucienā), rakt bedres, un varbūt dažreiz arī bišķi pamācīties.",
-      img: `../trails.jpg`,
+      img: `../${base}/trails.jpg`,
       years: 3
     },
     {
       slug: "mediki",
       name: "Medicīnas studenti...",
       description: "Mediķi... Tie kas pārdevuši savu dvēseli uz 6 gadiem + rezidentūru. Bet ja tu sevī jūti aicinājumu palīdzēt cilvēkiem, vai ceri uz labu algu pēc studiju pabeigšanas, varbūt ir tā vērts pamēģināt!",
-      img: `../trails.jpg`,
+      img: `../${base}/trails.jpg`,
       years: 0
     },
   ]
