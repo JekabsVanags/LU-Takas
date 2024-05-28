9aabe643c53497bf645d32c33627a7557eee6869
JekabsVanags <jecobv@gmail.com>
2024-05-28 15:48:44 +0300

    Updates

--- _app/immutable/chunks/stores.w4Coj0fk.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./paths.B5geyox3.js";const e=a(0);function c(t){e.update(s=>t)}const o=a(!1);function d(){o.update(t=>!t)}export{e as a,o as d,c as s,d as t};
---
+++ _app/immutable/entry/start.CUgp7dip.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.rBXIRRZG.js";export{t as start};
